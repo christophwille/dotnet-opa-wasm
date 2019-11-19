67e8edcd7edaa2b8c60746eec552591562c79aca
Christoph Wille <christoph.wille@gmail.com>
2019-11-19 09:21:18 +0100

    Small sample to list imports from wasm

---
+++ MinimalInteraction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using WasmerSharp;
+
+namespace MinimalInteraction
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Memory memory = Memory.Create(minPages: 5);
+			// Import memoryImport = new Import("env", "memory", memory);
+
+			byte[] wasm = File.ReadAllBytes("policy.wasm");
+			Module m = Module.Create(wasm);
+
+			Console.WriteLine("The loaded wasm has the following imports listed:");
+			foreach (ImportDescriptor import in m.ImportDescriptors)
+			{
+				Console.WriteLine($"import: {import.Kind} {import.ModuleName}::{import.Name} ");
+			}
+
+			Console.Read();
+		}
+	}
+}
---
+++ MinimalInteraction/README.md
@@ -0,0 +1,12 @@
+Output should be (for OPA 0.15.1 compiled WASM) like this:
+
+```
+The loaded wasm has the following imports listed:
+import: Function env::opa_abort
+import: Function env::opa_builtin0
+import: Function env::opa_builtin1
+import: Function env::opa_builtin2
+import: Function env::opa_builtin3
+import: Function env::opa_builtin4
+import: Memory env::memory
+```
