c6793d95d59e59d5591a3bbef2e42ee9c2aa6d9e
Christoph Wille <christoph.wille@gmail.com>
2020-03-05 20:50:40 +0100

    Shift functionality around, complete implementation of Load analogous to WasmerSharp impl.

---
+++ WasmTimeTest/OpaPolicy.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wasmtime;
+
+namespace WasmTimeTest
+{
+	public static class OpaExtensions
+	{
+		public static OpaPolicy CreateOpaPolicy(this Module module)
+		{
+			var op = new OpaPolicy();
+			op.Load(module);
+			return op;
+		}
+	}
+
+	public class OpaPolicy
+	{
+		private int _dataAddr;
+		private int _baseHeapPtr;
+		private int _baseHeapTop;
+		private int _dataHeapPtr;
+		private int _dataHeapTop;
+
+		private Instance _instance;
+		private OpaHost _host;
+		private dynamic _policy;
+
+		internal OpaPolicy()
+		{
+		}
+
+		internal void Load(Module module)
+		{
+			_host = new OpaHost();
+			_instance = module.Instantiate(_host);
+			_policy = (dynamic)_instance;
+
+			string builtins = DumpJson(_policy.builtins());
+
+			_dataAddr = LoadJson("{}");
+			_baseHeapPtr = _policy.opa_heap_ptr_get();
+			_baseHeapTop = _policy.opa_heap_top_get();
+			_dataHeapPtr = _baseHeapPtr;
+			_dataHeapTop = _baseHeapTop;
+
+			// endof js ctor LoadedPolicy
+		}
+
+		private int LoadJson(string json)
+		{
+			int addr = _policy.opa_malloc(json.Length);
+			_host.EnvMemory.WriteString(addr, json);
+
+			int parseAddr = _policy.opa_json_parse(addr, json.Length);
+
+			if (0 == parseAddr)
+			{
+				throw new ArgumentNullException("Parsing failed");
+			}
+
+			return parseAddr;
+		}
+
+		private string DumpJson(int addrResult)
+		{
+			int addr = _policy.opa_json_dump(addrResult);
+			return DecodeNullTerminatedString(_host.EnvMemory, addr);
+		}
+
+		private static string DecodeNullTerminatedString(Memory memory, int addr)
+		{
+			var buf = memory.Span;
+			int idx = addr;
+
+			while (buf[idx] != 0)
+			{
+				idx++;
+			}
+
+			return memory.ReadString(addr, idx - addr);
+		}
+	}
+}
--- WasmTimeTest/Program.cs
+++ WasmTimeTest/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Linq;
 using Wasmtime;
 using Wasmtime.Exports;
@@ -16,94 +15,8 @@ namespace WasmTimeTest
 			using var store = engine.CreateStore();
 			using var module = store.CreateModule("policy.wasm");
 
-			var host = new OpaHost();
-			using var instance = module.Instantiate(host);
+			var opaPolicy = module.CreateOpaPolicy();
 
-			dynamic policy = (dynamic) instance;
-			int addrBuiltins = policy.builtins();
-			int addrDumpedJson = policy.opa_json_dump(addrBuiltins);
-
-			var buf = host.EnvMemory.Span;
-			int idx = addrDumpedJson;
-
-			while (buf[idx] != 0)
-			{
-				idx++;
-			}
-
-			string dumpedJson = host.EnvMemory.ReadString(addrDumpedJson, idx - addrDumpedJson);
 		}
 	}
-
-	class OpaHost : IHost
-	{
-		public OpaHost()
-		{
-			EnvMemory = new Memory(2); // (import "env" "memory" (memory $env.memory 2))
-		}
-
-		public Instance Instance { get; set; }
-
-		[Import("memory", Module = "env")]
-		public readonly Memory EnvMemory;
-
-		[Import("opa_abort", Module = "env")]
-		public virtual void Abort(int addr)
-		{
-			Debugger.Break();
-			// TODO: impl stringDecoder
-		}
-
-		[Import("opa_builtin0", Module = "env")]
-		public virtual int Builtin0(int builtinId, int opaCtxReserved)
-		{
-			Debugger.Break();
-			return 0;
-		}
-
-		[Import("opa_builtin1", Module = "env")]
-		public virtual int Builtin1(int builtinId, int opaCtxReserved, int addr1)
-		{
-			Debugger.Break();
-			return 0;
-		}
-
-		[Import("opa_builtin2", Module = "env")]
-		public virtual int Builtin2(int builtinId, int opaCtxReserved, int addr1, int addr2)
-		{
-			Debugger.Break();
-			return 0;
-		}
-
-		[Import("opa_builtin3", Module = "env")]
-		public virtual int Builtin3(int builtinId, int opaCtxReserved, int addr1, int addr2, int addr3)
-		{
-			Debugger.Break();
-			return 0;
-		}
-
-		[Import("opa_builtin4", Module = "env")]
-		public virtual int Builtin4(int builtinId, int opaCtxReserved, int addr1, int addr2, int addr3, int addr4)
-		{
-			Debugger.Break();
-			return 0;
-		}
-	}
-
-	/* https://webassembly.github.io/wabt/demo/wasm2wat/
-
-	  (type $t0 (func (param i32 i32) (result i32)))
-	  (type $t2 (func (param i32 i32 i32) (result i32)))
-	  (type $t3 (func (param i32)))
-	  (type $t4 (func (param i32 i32 i32 i32) (result i32)))
-	  (type $t5 (func (param i32 i32 i32 i32 i32) (result i32)))
-	  (type $t6 (func (param i32 i32 i32 i32 i32 i32) (result i32)))
-	  (import "env" "memory" (memory $env.memory 2))
-	  (import "env" "opa_abort" (func $env.opa_abort (type $t3)))
-	  (import "env" "opa_builtin0" (func $env.opa_builtin0 (type $t0)))
-	  (import "env" "opa_builtin1" (func $env.opa_builtin1 (type $t2)))
-	  (import "env" "opa_builtin2" (func $env.opa_builtin2 (type $t4)))
-	  (import "env" "opa_builtin3" (func $env.opa_builtin3 (type $t5)))
-	  (import "env" "opa_builtin4" (func $env.opa_builtin4 (type $t6)))
-	*/
 }
