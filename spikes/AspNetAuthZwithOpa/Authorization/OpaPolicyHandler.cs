3b24f9cd8fa5172afab5bf08300aa89527ea6c65
Christoph Wille <christoph.wille@gmail.com>
2020-03-09 14:28:33 +0100

    Sample for evaluating OPA policies in an ASP.NET Authorization Policy - the reason this whole project started.

---
+++ spikes/AspNetAuthZwithOpa/Authorization/OpaPolicyHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Opa.Wasm;
+using Wasmtime;
+
+namespace AspNetAuthZwithOpa.Authorization
+{
+	public class OpaPolicyHandler : AuthorizationHandler<OpaPolicyRequirement>
+	{
+		private readonly IWebHostEnvironment _env;
+		private readonly Store _store;
+		private readonly string _wasmLoadPath;
+
+		public OpaPolicyHandler(IWebHostEnvironment env)
+		{
+			_env = env;
+			var engine = new Engine();
+			_store = engine.CreateStore();
+
+			// Yes, this isn't a nice way, but loading from disk on every request wouldn't be production-ready either
+			_wasmLoadPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+
+		}
+		protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, OpaPolicyRequirement requirement)
+		{
+			// Only shows the concept, you'd need to: (1) cache wasms, (2) json-in, json-out, (3) evaluate out-json
+			//       json-in: a defined set of properties of the request that is then being evaluated by the OPA policy (treated as black box)
+			// Might need a https://docs.microsoft.com/en-us/aspnet/core/security/authorization/iauthorizationpolicyprovider?view=aspnetcore-3.1
+
+			string policy = requirement.Policy;
+
+			using var module = _store.CreateModule(System.IO.Path.Combine(_wasmLoadPath, policy + ".wasm"));
+			var opaPolicy = module.CreateOpaPolicy();
+
+			opaPolicy.SetData(@"{""world"": ""world""}");
+
+			string input = @"{""message"": ""world""}";
+			string output = opaPolicy.Evaluate(input);
+
+			context.Succeed(requirement);
+
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ spikes/AspNetAuthZwithOpa/Authorization/OpaPolicyRequirement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AspNetAuthZwithOpa.Authorization
+{
+	// https://docs.microsoft.com/en-us/aspnet/core/security/authorization/policies?view=aspnetcore-3.1
+	public class OpaPolicyRequirement : IAuthorizationRequirement
+	{
+		public string Policy { get; }
+
+		public OpaPolicyRequirement(string policy)
+		{
+			Policy = policy;
+		}
+	}
+}
