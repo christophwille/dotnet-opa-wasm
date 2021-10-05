960e7076845517cb149929e8e317527d2b2e168d
Christoph Wille <christoph.wille@gmail.com>
2021-10-05 11:27:03 +0200

    Added example-one.rego from https://github.com/open-policy-agent/npm-opa-wasm/tree/main/examples/nodejs-ts-app-multi-entrypoint/policies

--- sample-policies/build-on-windows.bat
+++ sample-policies/build-on-windows.bat
@@ -11,3 +11,8 @@ del policy.wasm
 del bundle.tar.gz
 
 .\opa_windows_amd64.exe build example.rego --target wasm --entrypoint "example/hello" --output bundle-example.tar.gz
+
+.\opa_windows_amd64 build -t wasm -e 'example' -e 'example/one' example-one.rego
+tar -xzf bundle.tar.gz /policy.wasm
+copy policy.wasm multi.wasm
+del policy.wasm
