1d07482fd7d0eb4968d3d884c0cba0c402558437
Christoph Wille <christoph.wille@gmail.com>
2021-11-02 10:11:08 +0100

    Fix #21 (It does matter how you pass cmd line arguments, at least starting with OPA 0.34)

--- sample-policies/build-on-windows.bat
+++ sample-policies/build-on-windows.bat
@@ -12,7 +12,7 @@ del bundle.tar.gz
 
 .\opa_windows_amd64.exe build example.rego --target wasm --entrypoint "example/hello" --output bundle-example.tar.gz
 
-.\opa_windows_amd64 build -t wasm -e 'example' -e 'example/one' example-one.rego
+.\opa_windows_amd64 build -t wasm -e "example" -e "example/one" example-one.rego
 tar -xzf bundle.tar.gz /policy.wasm
 copy policy.wasm multi.wasm
 del policy.wasm
