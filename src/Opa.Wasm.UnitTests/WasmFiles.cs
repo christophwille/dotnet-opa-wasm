b3182d603d82c80aaf5b52a028a45db30e78b674
Christoph Wille <christoph.wille@gmail.com>
2021-11-04 14:35:25 +0100

    Linux test runner obviously not happy about backslashes instead of forward slash

--- src/Opa.Wasm.UnitTests/WasmFiles.cs
+++ src/Opa.Wasm.UnitTests/WasmFiles.cs
@@ -2,14 +2,14 @@
 {
 	public static class WasmFiles
 	{
-		public static readonly string HelloWorldExample = "LinkedWasms\\example.wasm";
-		public static readonly string RbacExample = "LinkedWasms\\rbac.wasm";
+		public static readonly string HelloWorldExample = "LinkedWasms/example.wasm";
+		public static readonly string RbacExample = "LinkedWasms/rbac.wasm";
 
-		public static readonly string MultiEntrypointExample = "LinkedWasms\\multi.wasm";
+		public static readonly string MultiEntrypointExample = "LinkedWasms/multi.wasm";
 
-		public static readonly string BuiltinExample = "LinkedWasms\\simplebuiltincall.wasm";
-		public static readonly string AllBuiltinsExample = "LinkedWasms\\builtincall.wasm";
+		public static readonly string BuiltinExample = "LinkedWasms/simplebuiltincall.wasm";
+		public static readonly string AllBuiltinsExample = "LinkedWasms/builtincall.wasm";
 
-		public static readonly string AbortExample = "LinkedWasms\\abort.wasm";
+		public static readonly string AbortExample = "LinkedWasms/abort.wasm";
 	}
 }
