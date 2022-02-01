a79eb4ea292d771629c0db4500dafa46163af571
Christoph Wille <christoph.wille@gmail.com>
2022-02-01 16:27:19 +0100

    Add a bit more meat to bones

--- spikes/HigherLevelApisSpike/IPolicyFactory.cs
+++ spikes/HigherLevelApisSpike/IPolicyFactory.cs
@@ -8,11 +8,17 @@ public class Policy : IDisposable
 {
     private bool disposedValue;
 
+    internal Module? _module;
+    internal string _policyName;
+
     public OpaPolicy Opa { get; }
 
-    public Policy(PolicyFactory factory, OpaRuntime opaRuntime, OpaPolicy opaPolicy, Module module, string policyName)
+    public Policy(OpaPolicy opaPolicy, Module module, string policyName)
     {
         Opa = opaPolicy;
+
+        _module = module;
+        _policyName = policyName;
     }
 
     public void SetData(object o)
@@ -32,6 +38,11 @@ public class Policy : IDisposable
             if (disposing)
             {
                 // TODO: dispose managed state (managed objects)
+
+                // When disposing, we are not telling the PolicyFactory
+                Opa.Dispose();
+                if (null != _module) _module.Dispose();
+                _module = null;
             }
 
             // TODO: free unmanaged resources (unmanaged objects) and override finalizer
@@ -47,30 +58,58 @@ public class Policy : IDisposable
     }
 }
 
+public interface IPolicyStore
+{
+    Task<(byte[], bool)> LoadPolicyAsync(string name, bool throwOnLoadError = false);
+}
+
+public class DummyPolicyStore : IPolicyStore
+{
+    public Task<(byte[], bool)> LoadPolicyAsync(string name, bool throwOnLoadError = false)
+    {
+        throw new NotImplementedException();
+    }
+}
+
 public class PolicyFactory
 {
+    private readonly IPolicyStore _store;
+
     // inject cache if any, inject opa wasm loader, inject data service (SetData) if any
     // inject serializer if any
-    public PolicyFactory()
+    public PolicyFactory(IPolicyStore store)
     {
+        _store = store;
     }
 
     // Idea from ArrayPool
-    Policy Rent(string policyName)
+    public async Task<Policy> RentAsync(string policyName)
     {
         // disposing of those objects is the duty of the consumer
         var opaRuntime = new OpaRuntime();
+
+        // Non-cache case
+        var (wasmBytes, err) = await _store.LoadPolicyAsync(policyName);
+        var module = opaRuntime.Load(policyName, wasmBytes);
+
         var opaPolicy = new OpaPolicy(opaRuntime, module);
 
         // remove from cache if we are using it
 
         // opaPolicy.SetData - from either the Rent param or maybe a service?
 
-        return new Policy(this, opaRuntime, opaPolicy, );
+        return new Policy(opaPolicy, module, policyName);
     }
 
-    void Return(Policy p)
+    // intentionally void because the cache if any is going to be on the local machine!
+    public void Return(Policy p)
     {
         // re-add to cache if we do caching
+        var moduleToReturn = p._module;
+        p._module = null;
+
+        var nameofPolicy = p._policyName;
+
+        // DO MAGIC
     }
 }
--- spikes/HigherLevelApisSpike/Program.cs
+++ spikes/HigherLevelApisSpike/Program.cs
@@ -1,11 +1,13 @@
 ﻿using HigherLevelApisSpike;
 
-var factory = new PolicyFactory();
+var factory = new PolicyFactory(new DummyPolicyStore());
 
-var policy = factory.Rent("mysamplepolicy");
+var policy = await factory.RentAsync("mysamplepolicy");
 
 // if you want to be fancy use the string version on the root object
-policy.Opa.SetData("");
+// policy.Opa.SetData("");
 policy.SetData(new { blah = "blups" });
 
 policy.Evaluate(new { myName = "" });
+
+factory.Return(policy); // do not use policy after here, but return is optional re:IDisposable
