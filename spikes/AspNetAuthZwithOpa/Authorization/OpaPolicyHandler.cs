2c70b1240455fbd39807bcf4220c975e1faed4ab
Christoph Wille <christoph.wille@gmail.com>
2022-02-01 15:25:48 +0100

    Update spike

--- spikes/AspNetAuthZwithOpa/Authorization/OpaPolicyHandler.cs
+++ spikes/AspNetAuthZwithOpa/Authorization/OpaPolicyHandler.cs
@@ -7,44 +7,46 @@ using Opa.Wasm;
 
 namespace AspNetAuthZwithOpa.Authorization
 {
-	public class OpaPolicyHandler : AuthorizationHandler<OpaPolicyRequirement>
-	{
-		private readonly IPoliciesStore _policiesStore;
-		private readonly ILogger<OpaPolicyHandler> _logger;
+    public class OpaPolicyHandler : AuthorizationHandler<OpaPolicyRequirement>
+    {
+        private readonly IPoliciesStore _policiesStore;
+        private readonly ILogger<OpaPolicyHandler> _logger;
 
-		public OpaPolicyHandler(IPoliciesStore policiesStore, ILogger<OpaPolicyHandler> logger)
-		{
-			_policiesStore = policiesStore;
-			_logger = logger;
-		}
+        public OpaPolicyHandler(IPoliciesStore policiesStore, ILogger<OpaPolicyHandler> logger)
+        {
+            _policiesStore = policiesStore;
+            _logger = logger;
+        }
 
-		protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, OpaPolicyRequirement requirement)
-		{
-			// Only shows the concept, you'd need to: (1) cache wasms, (2) json-in, json-out, (3) evaluate out-json
-			//       json-in: a defined set of properties of the request that is then being evaluated by the OPA policy (treated as black box)
-			// Might need a https://docs.microsoft.com/en-us/aspnet/core/security/authorization/iauthorizationpolicyprovider?view=aspnetcore-3.1
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, OpaPolicyRequirement requirement)
+        {
+            // Only shows the concept, you'd need to: (1) cache wasms, (2) json-in, json-out, (3) evaluate out-json
+            //       json-in: a defined set of properties of the request that is then being evaluated by the OPA policy (treated as black box)
+            // Might need a https://docs.microsoft.com/en-us/aspnet/core/security/authorization/iauthorizationpolicyprovider?view=aspnetcore-3.1
 
-			string policyName = requirement.Policy;
-			var (wasmBytes, succeeded) = await _policiesStore.LoadPolicyAsync(policyName);
+            string policyName = requirement.Policy;
+            var (wasmBytes, succeeded) = await _policiesStore.LoadPolicyAsync(policyName);
 
-			if (succeeded)
-			{
-				using var opaModule = new OpaModule();
-				// TODO: This incurs the compilation penalty for wasm - use an object pool (single-threaded use only)
-				using var module = opaModule.Load(policyName, wasmBytes);
-				using var opaPolicy = new OpaPolicy(opaModule, module);
+            if (succeeded)
+            {
+                using var opaRuntime = new OpaRuntime();
 
-				opaPolicy.SetData(@"{""world"": ""world""}");
+                // TODO: This incurs the compilation penalty for wasm - use an object pool (single-threaded use only)
+                using var module = opaRuntime.Load(policyName, wasmBytes);
 
-				string input = @"{""message"": ""world""}";
-				string output = opaPolicy.Evaluate(input);
+                using var opaPolicy = new OpaPolicy(opaRuntime, module);
 
-				context.Succeed(requirement);
-			}
-			else
-			{
-				_logger.LogError($"Policy {policyName} not found, cannot evaluate");
-			}
-		}
-	}
+                opaPolicy.SetData(@"{""world"": ""world""}");
+
+                string input = @"{""message"": ""world""}";
+                string output = opaPolicy.Evaluate(input);
+
+                context.Succeed(requirement);
+            }
+            else
+            {
+                _logger.LogError($"Policy {policyName} not found, cannot evaluate");
+            }
+        }
+    }
 }
