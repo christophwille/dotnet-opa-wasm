7c550d6d12b049e3d69d39b544d4eb296fd3848d
Christoph Wille <christoph.wille@gmail.com>
2022-02-01 15:59:01 +0100

    Non-working draft of how a factory could look like

---
+++ spikes/HigherLevelApisSpike/IPolicyFactory.cs
@@ -0,0 +1,76 @@
+﻿using Opa.Wasm;
+using System.Text.Json;
+using Wasmtime;
+
+namespace HigherLevelApisSpike;
+
+public class Policy : IDisposable
+{
+    private bool disposedValue;
+
+    public OpaPolicy Opa { get; }
+
+    public Policy(PolicyFactory factory, OpaRuntime opaRuntime, OpaPolicy opaPolicy, Module module, string policyName)
+    {
+        Opa = opaPolicy;
+    }
+
+    public void SetData(object o)
+    {
+        this.Opa.SetData(JsonSerializer.Serialize(o));
+    }
+
+    public string Evaluate(object o)
+    {
+        return this.Opa.Evaluate(JsonSerializer.Serialize(o));
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
+
+public class PolicyFactory
+{
+    // inject cache if any, inject opa wasm loader, inject data service (SetData) if any
+    // inject serializer if any
+    public PolicyFactory()
+    {
+    }
+
+    // Idea from ArrayPool
+    Policy Rent(string policyName)
+    {
+        // disposing of those objects is the duty of the consumer
+        var opaRuntime = new OpaRuntime();
+        var opaPolicy = new OpaPolicy(opaRuntime, module);
+
+        // remove from cache if we are using it
+
+        // opaPolicy.SetData - from either the Rent param or maybe a service?
+
+        return new Policy(this, opaRuntime, opaPolicy, );
+    }
+
+    void Return(Policy p)
+    {
+        // re-add to cache if we do caching
+    }
+}
---
+++ spikes/HigherLevelApisSpike/Program.cs
@@ -0,0 +1,11 @@
+﻿using HigherLevelApisSpike;
+
+var factory = new PolicyFactory();
+
+var policy = factory.Rent("mysamplepolicy");
+
+// if you want to be fancy use the string version on the root object
+policy.Opa.SetData("");
+policy.SetData(new { blah = "blups" });
+
+policy.Evaluate(new { myName = "" });
